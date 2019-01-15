394beb63ca6f5f73b82471fc57c2c842a686e919
Leonardo Feliciano <cabelytos@gmail.com>
2019-01-15 11:22:14 -0200

    CRUD Scaffolding

---
+++ VendsWeb/Data/VendsWebContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace VendsWeb.Models
+{
+    public class VendsWebContext : DbContext
+    {
+        public VendsWebContext (DbContextOptions<VendsWebContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<VendsWeb.Models.Department> Department { get; set; }
+    }
+}
